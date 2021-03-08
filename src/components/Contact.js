1e59da28f4177c4835cb5091208cd0426cc4e16b
Sam Tovey <samtovey13@gmail.com>
2021-03-08 23:15:19 +0000

    added functionality to contact form with emailjs-com

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,82 @@
-import React from 'react';
+import React, { useState } from "react";
 import StyledContactForm from '../styled/ContactForm';
 import StyledPanel from '../styled/Panel';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  
+  const initialState = {
+    name: "",
+    email: "",
+    message: "",
+  };
+
+  const [fields, setFields] = useState(initialState);
+  const [success, setSuccess] = useState(false);
+  const [fail, setFail] = useState(false);
+
+  const handleChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSuccess(false);
+    setFail(false);
+    emailjs
+      .sendForm(
+        "contact-service",
+        "contact-form",
+        event.target,
+        "user_6ifIq0vO4Nx3KZmi56zJI"
+      )
+      .then(() => {
+        setSuccess(true);
+        setFields(initialState);
+      }, (error) => {
+        setFail(true);
+      });
+  }
+
   return (
     <StyledPanel className="contact">
       <h2>Contact</h2>
-      <StyledContactForm
-        name="contact-form"
-      >
-        <label for="name">Name</label>
-        <input type="text" name="name"></input>
+      <div className="alert">
+        {
+          success && <p className="success-message">Success! Message sent.</p>
+        }
+        {
+          fail && <p className="fail-message">Error! Message not sent.</p>
+        }
+      </div>
+      <StyledContactForm name="contact-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          required
+          type="text"
+          name="name"
+          value={fields.name}
+          onChange={handleChange}
+        ></input>
 
-        <label for="email">Email</label>
-        <input type="email" name="email"></input>
+        <label htmlFor="email">Email</label>
+        <input
+          required
+          type="email"
+          name="email"
+          value={fields.email}
+          onChange={handleChange}
+        ></input>
 
-        <label for="message">Message</label>
-        <textarea name="message" width="100%" rows="10"></textarea>
+        <label htmlFor="message">Message</label>
+        <textarea
+          required
+          name="message"
+          width="100%"
+          rows="10"
+          value={fields.message}
+          onChange={handleChange}
+        ></textarea>
 
         <button type="submit" className="contact-submit-button">
           Send
