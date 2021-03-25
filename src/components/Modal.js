cbc6c220c2b968f61123bfdcb5d21e4b57c88d7d
Sam Tovey <samtovey13@gmail.com>
2021-03-25 22:51:11 +0000

    uploaded gifs and imported to Modal template

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import StyledModal from "../styled/Modal";
+import StyledPanel from "../styled/Panel";
 
 
 const Modal = ({ props, hideModal, isOpen }) => {
@@ -22,9 +23,18 @@ const Modal = ({ props, hideModal, isOpen }) => {
     <StyledModal id="modal" onKeyUp={handleKeyUp} tabIndex={-1}>
       <div className="modal-overlay">
         <div className="modal-container">
-          <div>{props.header}</div>
-          <input type="text" onKeyUp={handleKeyUp} />
-          <button onClick={hideModal}>Hide</button>
+          <button className="x-button" onClick={hideModal}>
+            X
+          </button>
+          <StyledPanel>
+            <div className="panel-inner">
+              <div>{props.header}</div>
+              <img src={props.images.gif1} alt="some alt text" width="500px" />
+              <button className="hide-button" onClick={hideModal}>
+                Hide
+              </button>
+            </div>
+          </StyledPanel>
         </div>
       </div>
     </StyledModal>
--- src/static/projects.js
+++ src/static/projects.js
@@ -1,9 +1,18 @@
+// square card images
 import surrealEstateSq from "./images/surreal-estate/screenshot-sq.png";
 import weatherSq from "./images/weather/weather.png";
 import nasaSq from "./images/nasa/screenshot-sq.png";
 import musicLibSq from "./images/music-library/screenshot-sq.png";
 import bookLibSq from "./images/book-library/screenshot-sq.png";
 import cruiseShipsSq from "./images/cruise-ships/screenshot-sq.png";
+
+// gifs
+import surrealEstateSearchGif from "./images/surreal-estate/property-search.gif";
+import surrealEstateFavouritesGif from "./images/surreal-estate/property-favourites.gif";
+import weatherLocationGif from "./images/weather/weather-location.gif"
+import NASASearchGif from "./images/nasa/NASA-search.gif";
+import musicLibGetGif from "./images/music-library/music-lib-GET.gif";
+import cruiseShipsGif from "./images/cruise-ships/cruise-ships-play.gif";
 
 const projects = [
   {
@@ -12,6 +21,8 @@ const projects = [
     summary: "React Property search app with API and Facebook SSO",
     images: {
       square: surrealEstateSq,
+      gif1: surrealEstateSearchGif,
+      gif2: surrealEstateFavouritesGif,
     } ,
   },
   {
@@ -20,6 +31,7 @@ const projects = [
     summary: "React weather app with live API",
     images: {
       square: weatherSq,
+      gif1: weatherLocationGif,
     }
   },
   {
@@ -28,6 +40,7 @@ const projects = [
     summary: "React app with image gallery",
     images: {
       square: nasaSq,
+      gif1: NASASearchGif,
     }
   },
   {
@@ -36,6 +49,7 @@ const projects = [
     summary: "Artists, Songs and Albums",
     images: {
       square: musicLibSq,
+      gif1: musicLibGetGif,
     }
   },
   {
@@ -52,6 +66,7 @@ const projects = [
     summary: "Vanilla JavaScript app with retro UI",
     images: {
       square: cruiseShipsSq,
+      gif1: cruiseShipsGif,
     }
   },
 ];
